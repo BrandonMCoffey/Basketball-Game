e3c11babd8b4796e979234b03d34268a2ec96a46
Brandon Coffey <bmc939@gmail.com>
2025-10-15 15:52:07 -0500

    Better Hair Generation for Players

--- Assets/BasketballTricks/Scripts/AnimatedPlayer.cs
+++ Assets/BasketballTricks/Scripts/AnimatedPlayer.cs
@@ -8,13 +8,10 @@ public class AnimatedPlayer : MonoBehaviour
     [SerializeField] private PlayerData _playerData;
     [SerializeField] private SkinnedMeshRenderer _skinRenderer;
     [SerializeField] private Material _skinMaterial;
-    [SerializeField] private Material _hairMaterial;
-    [SerializeField] private List<GameObject> _hairPrefabs;
-    [SerializeField] private List<GameObject> _facialHairPrefabs;
-    [SerializeField, Range(0f, 1f)] private float _facialHairChance = 0.5f;
     [SerializeField] private Gradient _skinGradient;
-    [SerializeField] private Gradient _hairGradient;
     [SerializeField] private Transform _head;
+    [SerializeField] private Material _hairMaterial;
+    [SerializeField] private List<HairData> _hairPrefabs;
 
     public PlayerData PlayerData => _playerData;
 
@@ -24,28 +21,33 @@ public class AnimatedPlayer : MonoBehaviour
         //skinMat.color = Random.ColorHSV(0.05f, 0.2f, 0.0f, 0.25f, 0.5f, 1.0f);
         skinMat.color = _skinGradient.Evaluate(Random.value);
 
-        var hairMat = new Material(_hairMaterial);
-        //hairMat.color = Random.ColorHSV(0.0f, 0.15f, 0.3f, 0.5f, 0.05f, 0.7f);
-        hairMat.color = _hairGradient.Evaluate(Random.value);
-
         SwitchMaterial(_skinRenderer, skinMat, 0);
 
         if (_hairPrefabs.Count > 0)
         {
-            var facialHairPrefab = _hairPrefabs[Random.Range(0, _hairPrefabs.Count - 1)];
-            if (facialHairPrefab != null)
+            var hairData = _hairPrefabs[Random.Range(0, _hairPrefabs.Count - 1)];
+            if (hairData != null)
             {
-                var facialHair = Instantiate(facialHairPrefab, _head);
-                SwitchMaterial(facialHair.GetComponent<MeshRenderer>(), hairMat, 0);
-            }
-        }
-        if (Random.value <= _facialHairChance && _facialHairPrefabs.Count > 0)
-        {
-            var hairPrefab = _facialHairPrefabs[Random.Range(0, _facialHairPrefabs.Count - 1)];
-            if (hairPrefab != null)
-            {
-                var hair = Instantiate(hairPrefab, _head);
+                var hair = Instantiate(hairData.HairObject, transform, false);
+                hair.transform.SetParent(_head);
+                var hairMat = new Material(_hairMaterial);
+                hairMat.color = hairData.GetHairColor();
                 SwitchMaterial(hair.GetComponent<MeshRenderer>(), hairMat, 0);
+
+                var facialHairPrefab = hairData.GetFacialHair();
+                if (facialHairPrefab != null)
+                {
+                    var facialHair = Instantiate(facialHairPrefab, transform, false);
+                    facialHair.transform.SetParent(_head);
+                    SwitchMaterial(facialHair.GetComponent<MeshRenderer>(), hairMat, 0);
+                }
+
+                var headbandPrefab = hairData.GetHeadBand();
+                if (headbandPrefab != null)
+                {
+                    var headband = Instantiate(headbandPrefab, transform, false);
+                    headband.transform.SetParent(_head);
+                }
             }
         }
     }
---
+++ Assets/BasketballTricks/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private Vector3 _rotate = Vector3.up;
+    [SerializeField] private float _rotateSpeed = 5;
+
+    void Update()
+    {
+        transform.Rotate(_rotate, _rotateSpeed * Time.deltaTime);
+    }
+}
