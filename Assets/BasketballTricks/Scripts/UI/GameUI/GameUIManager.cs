1151d4100a9a1d9628e19ed90a41550134825084
Jacqueline Hinojosa <135283960+chriGauthier@users.noreply.github.com>
2025-10-30 15:02:01 -0500

    I broke only a few things....the crowd is not wild

--- Assets/BasketballTricks/Scripts/UI/GameUI/GameUIManager.cs
+++ Assets/BasketballTricks/Scripts/UI/GameUI/GameUIManager.cs
@@ -1,26 +1,78 @@
 using UnityEngine;
-using DG.Tweening;
-using System.Collections;
+using System.Collections.Generic;
 
+
+public enum TutorialState
+{
+    Intro,
+    Catalog,
+    DragPlayer,
+    SelectPlayer,
+    RemoveAction,
+    SelectOtherPlayer,
+    Simulate
+}
 public class GameUIManager : MonoBehaviour
 {
-    [SerializeField] MenuReference _slideInPanel;
-    [SerializeField] MenuReference _timelinePanel;
+    [Header("Settings")]
+    [SerializeField] bool _startTutorial = true;
 
-    IEnumerator Start()
+    [Header("Sliding Panels")]
+    [SerializeField] SlideInPanel _cardCatalog;
+    [SerializeField] SlideInPanel _timelinePanel;
+    [SerializeField] SlideInPanel _playerActionPanel;
+
+    [Header("TutorialPanels")]
+    [SerializeField] RectTransform _tutorialBlocker;
+    [SerializeField] TutorialState _currentTutorialState = TutorialState.Intro;
+    [SerializeField] List<RectTransform> _tutorialPanels = new List<RectTransform>();
+    void Start()
     {
-        yield return new WaitForSeconds(0.1f);
-        SetInitialOffScreenPositions();
+        if (_startTutorial && _tutorialPanels.Count > 0)
+        {
+            _tutorialBlocker.gameObject.SetActive(true);
+            foreach (var panel in _tutorialPanels)
+            {
+                panel.gameObject.SetActive(false);
+            }
 
-        yield return new WaitForSecondsRealtime(0.2f);
-        _slideInPanel.ShowPanel();
-        yield return new WaitForSecondsRealtime(0.2f);
-        _timelinePanel.ShowPanel();
+            _tutorialPanels[0].gameObject.SetActive(true);
+        }
+        else
+        {
+            _tutorialBlocker.gameObject.SetActive(false);
+        }
     }
 
-    private void SetInitialOffScreenPositions()
+    public void NextTutorialState()
     {
-        _slideInPanel.Panel.anchoredPosition = _slideInPanel.OffScreenPosition;
-        _timelinePanel.Panel.anchoredPosition = _timelinePanel.OffScreenPosition;
+        _currentTutorialState++;
+        switch (_currentTutorialState)
+        {
+            case TutorialState.Catalog:
+                _tutorialPanels[0].gameObject.SetActive(false);
+                _tutorialPanels[1].gameObject.SetActive(true);
+                _cardCatalog.transform.SetAsLastSibling();
+                _cardCatalog.SetShown(true);
+                break;
+            case TutorialState.DragPlayer:
+                _tutorialBlocker.gameObject.SetActive(false);
+                _tutorialPanels[1].gameObject.SetActive(false);
+                _tutorialPanels[2].gameObject.SetActive(true);
+                break;
+            case TutorialState.SelectPlayer:
+                _cardCatalog.SetShown(false);
+                _timelinePanel.SetShown(true);
+                _tutorialPanels[2].gameObject.SetActive(false);
+                _tutorialPanels[3].gameObject.SetActive(true);
+                _playerActionPanel.transform.SetAsLastSibling();
+                break;
+            case TutorialState.RemoveAction:
+                break;
+            case TutorialState.SelectOtherPlayer:
+                break;
+            case TutorialState.Simulate:
+                break;
+        }
     }
 }
