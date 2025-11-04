a725276c1b07e25b20db37312803e57be0fd416a
BrandonMCoffey <bmc939@gmail.com>
2025-11-04 15:18:23 -0600

    Bug fix player art

--- Assets/BasketballTricks/Scripts/PlayerArt.cs
+++ Assets/BasketballTricks/Scripts/PlayerArt.cs
@@ -9,6 +9,10 @@ public class PlayerArt : MonoBehaviour
     [SerializeField] private Vector3 _localHairPosition = new Vector3(0, -0.0159f, 0.00015f);
     [SerializeField] private Vector3 _localHairRotation = new Vector3(-90, 0, 0);
 
+    private GameObject _currentHair;
+    private GameObject _currentFacialHair;
+    private GameObject _currentHeadband;
+
     public void SetPlayerArt(PlayerArtData art)
     {
         var skinMat = new Material(_skinMaterial);
@@ -17,6 +21,10 @@ public class PlayerArt : MonoBehaviour
 
         SwitchMaterial(_skinRenderer, skinMat, 0);
 
+        if (_currentHair != null) Destroy(_currentHair);
+        if (_currentFacialHair != null) Destroy(_currentFacialHair);
+        if (_currentHeadband != null) Destroy(_currentHeadband);
+
         if (art.HairPrefab != null || art.FacialHairPrefab != null)
         {
             var hairMat = new Material(_hairMaterial);
@@ -24,29 +32,29 @@ public class PlayerArt : MonoBehaviour
 
             if (art.HairPrefab != null)
             {
-                var hair = Instantiate(art.HairPrefab, transform, false);
-                hair.transform.SetParent(_head);
-                hair.transform.SetLocalPositionAndRotation(_localHairPosition, Quaternion.Euler(_localHairRotation));
-                hair.layer = _skinRenderer.gameObject.layer;
-                SwitchMaterial(hair.GetComponent<MeshRenderer>(), hairMat, 0);
+                _currentHair = Instantiate(art.HairPrefab, transform, false);
+                _currentHair.transform.SetParent(_head);
+                _currentHair.transform.SetLocalPositionAndRotation(_localHairPosition, Quaternion.Euler(_localHairRotation));
+                _currentHair.layer = _skinRenderer.gameObject.layer;
+                SwitchMaterial(_currentHair.GetComponent<MeshRenderer>(), hairMat, 0);
             }
 
             if (art.FacialHairPrefab != null)
             {
-                var facialHair = Instantiate(art.FacialHairPrefab, transform, false);
-                facialHair.transform.SetParent(_head);
-                facialHair.transform.SetLocalPositionAndRotation(_localHairPosition, Quaternion.Euler(_localHairRotation));
-                facialHair.layer = _skinRenderer.gameObject.layer;
-                SwitchMaterial(facialHair.GetComponent<MeshRenderer>(), hairMat, 0);
+                _currentFacialHair = Instantiate(art.FacialHairPrefab, transform, false);
+                _currentFacialHair.transform.SetParent(_head);
+                _currentFacialHair.transform.SetLocalPositionAndRotation(_localHairPosition, Quaternion.Euler(_localHairRotation));
+                _currentFacialHair.layer = _skinRenderer.gameObject.layer;
+                SwitchMaterial(_currentFacialHair.GetComponent<MeshRenderer>(), hairMat, 0);
             }
         }
 
         if (art.AccessoryPrefab != null)
         {
-            var headband = Instantiate(art.AccessoryPrefab, transform, false);
-            headband.transform.SetParent(_head);
-            headband.transform.SetLocalPositionAndRotation(_localHairPosition, Quaternion.Euler(_localHairRotation));
-            headband.layer = _skinRenderer.gameObject.layer;
+            _currentHeadband = Instantiate(art.AccessoryPrefab, transform, false);
+            _currentHeadband.transform.SetParent(_head);
+            _currentHeadband.transform.SetLocalPositionAndRotation(_localHairPosition, Quaternion.Euler(_localHairRotation));
+            _currentHeadband.layer = _skinRenderer.gameObject.layer;
         }
     }
 
