14717491ce04603c25332bace36a535b5a5085ba
Sai Mangipudi <thegeekskm101@gmail.com>
2025-11-24 17:36:24 -0600

    Added Sound System
    brutally cannibalized brandon's sound system...it's **our** sound system now

--- Assets/Plugins/CoffeyUtils/Runtime/Sound/Managers/SoundManager.cs
+++ Assets/Plugins/CoffeyUtils/Runtime/Sound/Managers/SoundManager.cs
@@ -12,6 +12,8 @@ namespace CoffeyUtils.Sound
         public const string DefaultSfxPlayerName = "SFX Player";
         
         [SerializeField] private bool _scenePersistent;
+        [SerializeField] private SoundLibrarySO _soundLibrary;
+        public SoundLibrarySO SoundLibrary => _soundLibrary;
         [SerializeField, ReadOnly] private MusicPool _musicPool;
         [SerializeField, ReadOnly] private SfxPool _sfxPool;
 
@@ -81,16 +83,24 @@ namespace CoffeyUtils.Sound
             _sfxPool.BuildInitialPool();
         }
 
+
         public static SfxPlayer PlaySfx(AudioClip clip) => Sfx.Play(clip);
         public static SfxPlayer PlaySfx(Sfx2dProp properties2d) => Sfx.Play(properties2d);
         // Redundant way of playing sfx if you prefer
         public static SfxPlayer PlaySfx(SfxBase sfx) => sfx.PlayGetPlayer();
         public static SfxPlayer PlaySfx(SfxReference sfx) => sfx.Play();
+
         public static void StopAllSfx() => Sfx.StopAll();
 
         public static MusicPlayer PlayMusicNow(MusicTrack track) => Music.Play(track);
         public static MusicPlayer PlayMusicNow(MusicTrack track, float delay) => Music.Play(track);
         public static void QueueMusic(MusicTrack track) => Music.QueueTrack(track);
         public static void StopAllMusic() => Music.StopAll();
+
+
+        // Sai's Weird Code...
+        public static SfxPlayer PlaySfx(SFXEventsEnum sfxEvent) => PlaySfx(Instance.SoundLibrary.GetSFXClip(sfxEvent));
+        public static MusicPlayer PlayMusicNow(MusicTracksEnum musicTrack) => PlayMusicNow(Instance.SoundLibrary.GetMusicTrack(musicTrack));
+        public static MusicPlayer PlayMusicNow(MusicTracksEnum musicTrack, float delay) => PlayMusicNow(Instance.SoundLibrary.GetMusicTrack(musicTrack), delay);
     }
 }
---
+++ Assets/Plugins/CoffeyUtils/Runtime/Sound/SO/SoundLibrarySO.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using CoffeyUtils.Sound;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(menuName = "Sound System/Sound Library")]
+public class SoundLibrarySO : ScriptableObject
+{
+    [SerializeField] private List<SFXEventClip> _sfxEventClips = new List<SFXEventClip>();
+    [SerializeField] private List<MusicTrackClip> _musicTrackClips = new List<MusicTrackClip>();
+
+    
+    public SfxBase GetSFXClip(SFXEventsEnum sfxEvent)
+    {
+        foreach (var sfxEventClip in _sfxEventClips)
+        {
+            if (sfxEventClip.Event == sfxEvent)
+            {
+                return sfxEventClip.Clip;
+            }
+        }
+        Debug.LogError($"[SoundLibrarySO] SFX Event '{sfxEvent}' not found in Sound Library '{name}'.");
+        return null;
+    }
+
+    public MusicTrack GetMusicTrack(MusicTracksEnum musicTrack)
+    {
+        foreach (var musicTrackClip in _musicTrackClips)
+        {
+            if (musicTrackClip.Track == musicTrack)
+            {
+                return musicTrackClip.Clip;
+            }
+        }
+        Debug.LogError($"[SoundLibrarySO] Music Track '{musicTrack}' not found in Sound Library '{name}'.");
+        return null;
+    }
+
+    [Button, Tooltip("EDITOR ONLY: Removes duplicate SFXEvents and MusicTracks, keeping the first occurrence for each.")]
+    public void RemoveDuplicates()
+    {
+        HashSet<SFXEventsEnum> seenEvents = new HashSet<SFXEventsEnum>();
+        for (int i = _sfxEventClips.Count - 1; i >= 0; i--)
+        {
+            if (seenEvents.Contains(_sfxEventClips[i].Event))
+            {
+                _sfxEventClips.RemoveAt(i);
+            }
+            else
+            {
+                seenEvents.Add(_sfxEventClips[i].Event);
+            }
+        }
+
+        HashSet<MusicTracksEnum> seenTracks = new HashSet<MusicTracksEnum>();
+        for (int i = _musicTrackClips.Count - 1; i >= 0; i--)
+        {
+            if (seenTracks.Contains(_musicTrackClips[i].Track))
+            {
+                _musicTrackClips.RemoveAt(i);
+            }
+            else
+            {
+                seenTracks.Add(_musicTrackClips[i].Track);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public struct SFXEventClip
+{
+    public SFXEventsEnum Event;
+    public SfxBase Clip;
+}
+
+[System.Serializable]
+public struct MusicTrackClip
+{
+    public MusicTracksEnum Track;
+    public MusicTrack Clip;
+}
