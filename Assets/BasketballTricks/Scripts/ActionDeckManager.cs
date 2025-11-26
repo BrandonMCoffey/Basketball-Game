2520bebe1e303bcc7e79b8be0e83d07f9886c9fd
Brandon Coffey <bmc939@gmail.com>
2025-11-25 20:00:51 -0600

    wip

--- Assets/BasketballTricks/Scripts/ActionDeckManager.cs
+++ Assets/BasketballTricks/Scripts/ActionDeckManager.cs
@@ -1,15 +1,31 @@
+using DG.Tweening;
 using SaiUtils.Extensions;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ActionDeckManager : MonoBehaviour
+public class ActionDeckManager : MonoBehaviour//, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
 {
     [SerializeField] private ActionCard _cardPrefab;
     [SerializeField] private int _handSize = 5;
-    [SerializeField] private Transform _handPosition;
+    [SerializeField] private Transform _cardContainer;
+    //[SerializeField] private RectTransform _selectionBoxVisual;
 
+    [Header("Arc Layout")]
+    [SerializeField] private float _arcRadius = 800f;
+    [SerializeField] private float _arcAngleSpread = 40f;
+    [SerializeField] private Vector2 _centerOffset = new Vector2(0, -600);
+    [SerializeField] private float _selectedHeightOffset = 100f;
+
+    [Header("Animation")]
+    [SerializeField] private float _layoutDuration = 0.3f;
+    [SerializeField] private Ease _layoutEase = Ease.OutBack;
+
+    private List<ActionCard> _cards = new List<ActionCard>();
     private List<ActionDeckCard> _actionDeck = new List<ActionDeckCard>();
+    
+    //private Vector2 _startDragPos;
+    //private RectTransform _selectionRectTr;
 
     public void Init()
     {
@@ -25,15 +41,136 @@ public class ActionDeckManager : MonoBehaviour
         _actionDeck.Shuffle();
 
         int count = Mathf.Min(_handSize, _actionDeck.Count);
+        _cards = new List<ActionCard>(count);
         for (int i = 0; i < count; i++)
         {
-            var actionCard = Instantiate(_cardPrefab, _handPosition, false);
+            var actionCard = Instantiate(_cardPrefab, _cardContainer, false);
             var pos = actionCard.transform.position;
-            pos.x = _handPosition.position.x + (i - 2) * 300f;
+            pos.x = _cardContainer.position.x + (i - 2) * 300f;
             actionCard.transform.position = pos;
-            actionCard.Init(_actionDeck[i].Player.CardData.GetAction(_actionDeck[i].ActionIndex), _actionDeck[i].Player.PositionColor);
+            actionCard.Init(_actionDeck[i].Player.CardData.GetAction(_actionDeck[i].ActionIndex), _actionDeck[i].Player.PositionColor, this);
+            _cards.Add(actionCard);
+        }
+        //_selectionRectTr = _selectionBoxVisual;
+        //_selectionBoxVisual.gameObject.SetActive(false);
+    }
+
+    public void UpdateCardLayout(ActionCard draggingCard = null)
+    {
+        int count = _cards.Count;
+        float angleStep = count > 1 ? _arcAngleSpread / (count - 1) : 0;
+        float currentAngle = -_arcAngleSpread / 2;
+
+        for (int i = 0; i < count; i++)
+        {
+            ActionCard card = _cards[i];
+
+            // 1. Calculate Arc Position
+            float rad = (currentAngle + 90) * Mathf.Deg2Rad;
+            Vector2 pos = new Vector2(
+                Mathf.Cos(rad) * _arcRadius,
+                Mathf.Sin(rad) * _arcRadius
+            ) + _centerOffset;
+
+            // 2. Add Selection Offset
+            if (card.IsSelected && card != draggingCard)
+            {
+                pos += Vector2.up * _selectedHeightOffset;
+            }
+
+            Quaternion rot = Quaternion.Euler(0, 0, currentAngle);
+
+            // 3. Apply
+            if (card != draggingCard)
+            {
+                card.UpdateTransform(pos, rot, _layoutDuration, _layoutEase);
+            }
+            else
+            {
+                card.UpdateRotationOnly(rot, 0.1f);
+            }
+
+            currentAngle += angleStep;
         }
     }
+
+    public void OnCardDragReorder(ActionCard draggedCard)
+    {
+        int draggedIndex = _cards.IndexOf(draggedCard);
+        int newIndex = draggedIndex;
+
+        for (int i = 0; i < _cards.Count; i++)
+        {
+            if (i == draggedIndex) continue;
+            if (draggedCard.transform.localPosition.x > _cards[i].transform.localPosition.x && draggedIndex < i) newIndex = i;
+            else if (draggedCard.transform.localPosition.x < _cards[i].transform.localPosition.x && draggedIndex > i) newIndex = i;
+        }
+
+        if (newIndex != draggedIndex)
+        {
+            _cards.RemoveAt(draggedIndex);
+            _cards.Insert(newIndex, draggedCard);
+            UpdateCardLayout(draggedCard);
+        }
+    }
+
+    /*
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        foreach (var card in _cards) card.SetSelected(false);
+        UpdateCardLayout();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _selectionBoxVisual.gameObject.SetActive(true);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_cardContainer as RectTransform, eventData.position, eventData.pressEventCamera, out _startDragPos);
+        UpdateSelectionBox(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        UpdateSelectionBox(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _selectionBoxVisual.gameObject.SetActive(false);
+        ApplyBoxSelection();
+    }
+
+    private void UpdateSelectionBox(PointerEventData eventData)
+    {
+        Vector2 currentPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_cardContainer as RectTransform, eventData.position, eventData.pressEventCamera, out currentPos);
+
+        float width = currentPos.x - _startDragPos.x;
+        float height = currentPos.y - _startDragPos.y;
+
+        _selectionRectTr.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+        _selectionRectTr.anchoredPosition = _startDragPos + new Vector2(width / 2, height / 2);
+    }
+
+    private void ApplyBoxSelection()
+    {
+        Vector3[] corners = new Vector3[4];
+        _selectionBoxVisual.GetWorldCorners(corners);
+        Rect selectionRect = new Rect(corners[0].x, corners[0].y, corners[2].x - corners[0].x, corners[2].y - corners[0].y);
+
+        foreach (var card in _cards)
+        {
+            Vector3[] cardCorners = new Vector3[4];
+            ((RectTransform)card.transform).GetWorldCorners(cardCorners);
+            Vector2 cardCenter = (cardCorners[0] + cardCorners[2]) / 2;
+
+            if (selectionRect.Contains(cardCenter))
+            {
+                card.SetSelected(true);
+            }
+        }
+        UpdateCardLayout();
+    }
+    */
 }
 
 public struct ActionDeckCard
--- Assets/BasketballTricks/Scripts/UI/Cards/ActionCard.cs
+++ Assets/BasketballTricks/Scripts/UI/Cards/ActionCard.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ActionCard : MonoBehaviour
+public class ActionCard : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     [SerializeField] private ActionData _actionData;
     [SerializeField] private Image _colorImage;
@@ -15,7 +15,28 @@ public class ActionCard : MonoBehaviour
     [SerializeField] private TMP_Text _actionHype;
     [SerializeField] private Image _actionIcon;
 
-    public void Init(ActionData data, Color color)
+    [Header("Drag")]
+    [SerializeField] private float _selectScale = 1.1f;
+    [SerializeField] private float _dragFollowSpeed = 20f;
+
+    private ActionDeckManager _manager;
+    private RectTransform _rectTransform;
+    private Canvas _canvas;
+    private bool _isDragging;
+    private bool _wasDragged;
+    private Tween _moveTween;
+    private Tween _rotTween;
+    private bool _isSelected;
+
+    public bool IsSelected => _isSelected;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _canvas = GetComponentInParent<Canvas>();
+    }
+
+    public void Init(ActionData data, Color color, ActionDeckManager manager)
     {
         _actionData = data;
         if (_actionName != null) _actionName.text = data.Name;
@@ -25,5 +46,73 @@ public class ActionCard : MonoBehaviour
         if (_actionHype != null) _actionHype.text = data.HypeGain.GetValue(data.ActionLevel).ToString("0");
         if (_actionIcon != null) _actionIcon.sprite = data.Icon;
         if (_colorImage != null) _colorImage.color = color;
+        _manager = manager;
+    }
+
+    public void SetSelected(bool selected)
+    {
+        _isSelected = selected;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _wasDragged = false;
+        _rectTransform.SetAsLastSibling();
+        _rectTransform.DOScale(_selectScale, 0.2f);
+
+        _moveTween?.Kill();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _isDragging = true;
+        _wasDragged = true;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _rectTransform.parent as RectTransform,
+            eventData.position,
+            _canvas.worldCamera,
+            out Vector2 localPoint
+        );
+
+        _rectTransform.localPosition = Vector3.Lerp(
+            _rectTransform.localPosition,
+            localPoint,
+            Time.deltaTime * _dragFollowSpeed
+        );
+
+        _manager.OnCardDragReorder(this);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _isDragging = false;
+        _rectTransform.DOScale(1f, 0.2f);
+
+        if (!_wasDragged)
+        {
+            _isSelected = !_isSelected;
+        }
+
+        _manager.UpdateCardLayout();
+    }
+
+    public void UpdateTransform(Vector2 targetPos, Quaternion targetRot, float duration, Ease ease)
+    {
+        _moveTween?.Kill();
+        _rotTween?.Kill();
+
+        if (!_isDragging)
+        {
+            _moveTween = _rectTransform.DOLocalMove(targetPos, duration).SetEase(ease);
+        }
+
+        _rotTween = _rectTransform.DOLocalRotateQuaternion(targetRot, duration);
+    }
+
+    public void UpdateRotationOnly(Quaternion targetRot, float duration)
+    {
+        _rotTween?.Kill();
+        _rotTween = _rectTransform.DOLocalRotateQuaternion(targetRot, duration);
     }
 }
