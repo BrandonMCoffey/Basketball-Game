c495a361ed266d93b2d1920b458bf9cb5707541e
BrandonMCoffey <bmc939@gmail.com>
2025-11-30 13:43:28 -0600

    Better player art showcase

--- Assets/BasketballTricks/Scripts/Art/PlayerArtShowcase.cs
+++ Assets/BasketballTricks/Scripts/Art/PlayerArtShowcase.cs
@@ -1,12 +1,12 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
 public class PlayerArtShowcase : MonoBehaviour
 {
-    [SerializeField] private List<PlayerData> _playerDatas;
-    [SerializeField] private List<PlayerData> _playerDatas2;
+    [SerializeField] private List<PlayerData> _playerDatasActive = new List<PlayerData>();
+    [SerializeField] private List<PlayerData> _playerDatasUnused = new List<PlayerData>();
+    [SerializeField] private List<PlayerData> _playerDatasNoArt = new List<PlayerData>();
 
     [Header("References")]
     [SerializeField] private Vector3 _spacing = Vector3.right;
@@ -20,28 +20,21 @@ public class PlayerArtShowcase : MonoBehaviour
             _playerRefs[i].transform.localPosition = i * _spacing;
         }
         int refIndex = 0;
-        for (int i = 0; i < _playerDatas.Count; i++)
+        void DisplayPlayer(PlayerData data, Color color)
         {
-            if (refIndex < _playerRefs.Count && _playerDatas[i] != null && _playerDatas[i].HasArtData)
+            if (refIndex < _playerRefs.Count && data != null)
             {
                 _playerRefs[refIndex].gameObject.SetActive(true);
-                _playerRefs[refIndex].PlayerArt.SetPlayerArt(_playerDatas[i].ArtData);
-                _playerRefs[refIndex].SetPositionColor(Color.green);
-                _playerRefs[refIndex].SetActionText(_playerDatas[i].PlayerName.Replace(" ", "\n"), -1);
+                _playerRefs[refIndex].PlayerArt.SetPlayerArt(data.ArtData);
+                _playerRefs[refIndex].SetPositionColor(color);
+                _playerRefs[refIndex].SetActionText(data.PlayerName.Replace(" ", "\n"), -1);
                 refIndex++;
             }
         }
-        for (int i = 0; i < _playerDatas2.Count; i++)
-        {
-            if (refIndex < _playerRefs.Count && _playerDatas2[i] != null && _playerDatas2[i].HasArtData)
-            {
-                _playerRefs[refIndex].gameObject.SetActive(true);
-                _playerRefs[refIndex].PlayerArt.SetPlayerArt(_playerDatas2[i].ArtData);
-                _playerRefs[refIndex].SetPositionColor(Color.cyan);
-                _playerRefs[refIndex].SetActionText(_playerDatas2[i].PlayerName.Replace(" ", "\n"), -1);
-                refIndex++;
-            }
-        }
+        for (int i = 0; i < _playerDatasActive.Count; i++) DisplayPlayer(_playerDatasActive[i], Color.green);
+        for (int i = 0; i < _playerDatasUnused.Count; i++) DisplayPlayer(_playerDatasUnused[i], Color.yellow);
+        for (int i = 0; i < _playerDatasNoArt.Count; i++) DisplayPlayer(_playerDatasNoArt[i], Color.red);
+
         for (int i = refIndex; i < _playerRefs.Count; i++)
         {
             _playerRefs[i].gameObject.SetActive(false);
