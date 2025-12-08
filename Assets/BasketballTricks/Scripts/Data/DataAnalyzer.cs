4f389fb7152d4a18a29ae7a0550df8fbd6478dea
BrandonMCoffey <bmc939@gmail.com>
2025-12-07 22:53:44 -0600

    Data analyzer tool

---
+++ Assets/BasketballTricks/Scripts/Data/DataAnalyzer.cs
@@ -0,0 +1,120 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataAnalyzer : ScriptableObject
+{
+    [SerializeField] private bool _showActionsUnderCards;
+    [SerializeField] private List<PlayerCardData> _cardsToAnalyze = new List<PlayerCardData>();
+    [SerializeField] private List<CardDataAnalysis> _cardsAnalysis = new List<CardDataAnalysis>();
+
+    [Space(20)]
+    [SerializeField] private bool _showCardsUnderActions;
+    [SerializeField] private List<PlayerActionData> _actionsToAnalyze = new List<PlayerActionData>();
+    [SerializeField] private List<ActionDataAnalysis> _actionsAnalysis = new List<ActionDataAnalysis>();
+
+    [System.Serializable]
+    private struct CardDataAnalysis
+    {
+        public PlayerCardData Card;
+        [ReadOnly, HideLabel] public string Details;
+        [ReadOnly, ShowIf(nameof(ShowActionsUnderCards)), ListDrawerSettings(ShowFoldout = false)]
+        public List<string> Actions;
+        [HideInInspector] public bool ShowActionsUnderCards;
+    }
+
+    [System.Serializable]
+    private struct ActionDataAnalysis
+    {
+        public PlayerActionData Action;
+        [ReadOnly, HideLabel] public string Details;
+        [ReadOnly, ShowIf(nameof(ShowCardsUnderActions)), ListDrawerSettings(ShowFoldout = true)]
+        public List<string> CardsUsingThis;
+        [HideInInspector] public bool ShowCardsUnderActions;
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        AnalyzeData();
+    }
+
+    [Button]
+    private void AnalyzeData()
+    {
+        _cardsAnalysis = new List<CardDataAnalysis>(_cardsToAnalyze.Count);
+        foreach (var card in _cardsToAnalyze)
+        {
+            List<string> list = new List<string>();
+            var cardActions = card.GetAllActions();
+            int count = 0;
+            foreach (var action in cardActions)
+            {
+                var actionData = action.GetData();
+                list.Add($"{action.Count}x | {actionData.Name} ({actionData.AssociatedRarity})");
+                count += action.Count;
+            }
+            _cardsAnalysis.Add(new CardDataAnalysis
+            {
+                Card = card,
+                Details = $"{card.Rarity} card with {count} actions.",
+                Actions = list,
+                ShowActionsUnderCards = _showActionsUnderCards
+            });
+        }
+
+        _actionsAnalysis = new List<ActionDataAnalysis>(_actionsToAnalyze.Count);
+        foreach (var action in _actionsToAnalyze)
+        {
+            List<string> list = new List<string>();
+            int cardCount = 0;
+            int totalCount = 0;
+            foreach (var card in _cardsToAnalyze)
+            {
+                var cardActions = card.GetAllActions();
+                foreach (var cardAction in cardActions)
+                {
+                    if (cardAction.GetActionSO() == action)
+                    {
+                        list.Add($"{cardAction.Count}x | {card.PlayerData.PlayerName} | {card.CardTitle} ({card.Rarity})");
+                        cardCount++;
+                        totalCount += cardAction.Count;
+                    }
+                }
+            }
+            _actionsAnalysis.Add(new ActionDataAnalysis
+            {
+                Action = action,
+                Details = $"{action.Data.AssociatedRarity}-level action found in {cardCount} cards ({totalCount} copies).",
+                CardsUsingThis = list,
+                ShowCardsUnderActions = _showCardsUnderActions
+            });
+        }
+    }
+
+    [Button]
+    private void FetchAllCardsAndActions()
+    {
+        _cardsToAnalyze = GetAllInstancesOfType<PlayerCardData>();
+        _actionsToAnalyze = GetAllInstancesOfType<PlayerActionData>();
+    }
+
+    public static List<T> GetAllInstancesOfType<T>() where T : ScriptableObject
+    {
+        List<T> instances = new List<T>();
+        string[] guids = UnityEditor.AssetDatabase.FindAssets("t:" + typeof(T).Name);
+
+        foreach (string guid in guids)
+        {
+            string assetPath = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+            T asset = UnityEditor.AssetDatabase.LoadAssetAtPath<T>(assetPath);
+            if (asset != null)
+            {
+                instances.Add(asset);
+            }
+        }
+        return instances;
+    }
+#endif
+}
--- Assets/BasketballTricks/Scripts/Data/PlayerCardData.cs
+++ Assets/BasketballTricks/Scripts/Data/PlayerCardData.cs
@@ -11,9 +11,11 @@ public class PlayerCardData : ScriptableObject
     [SerializeField] private string _cardTitle;
     [SerializeField] private CardRarity _rarity;
 
-    [SerializeField] private List<CustomPlayerAction> _actions = new List<CustomPlayerAction>();
+    [SerializeField] public List<CustomPlayerAction> _actions = new List<CustomPlayerAction>();
 
     public PlayerData PlayerData => _playerData;
+    public string CardTitle => _cardTitle;
+    public CardRarity Rarity => _rarity;
 
     public int ActionCount => _actions.Count();
     public int GetActionCount(int index)
@@ -26,12 +28,13 @@ public class PlayerCardData : ScriptableObject
         if (index >= _actions.Count || index < 0) return new ActionData(index == 2 ? ActionType.Shot : (index == 1 ? ActionType.Pass : ActionType.Trick));
         return _actions[index].GetData();
     }
+    public List<CustomPlayerAction> GetAllActions() => new List<CustomPlayerAction>(_actions);
 
     private void OnValidate()
     {
         for (int i = 0; i < _actions.Count; i++)
         {
-            _actions[i].DataPreview = _actions[i].GetData();
+            _actions[i].UpdateDataPreview();
         }
     }
 }
@@ -42,15 +45,17 @@ public class CustomPlayerAction
     [SerializeField] private PlayerActionData _action;
     [SerializeField] private LevelableFloat _count = new LevelableFloat(false, 1, 1, 1);
     [SerializeField] private Sprite _imageOverride;
-    [ReadOnly] public ActionData DataPreview;
+    [SerializeField, ReadOnly] private ActionData _dataPreview;
 
     public int Count => Mathf.RoundToInt(_action != null ? _count.GetValue(_action.Data.ActionLevel) : _count.GetValue(1));
+    public PlayerActionData GetActionSO() => _action;
     public ActionData GetData()
     {
         ActionData data = _action != null ? _action.Data : new ActionData(ActionType.Trick);
         if (_imageOverride != null) data.Icon = _imageOverride;
         return data;
     }
+    public void UpdateDataPreview() => _dataPreview = GetData();
 }
 
 public enum CardRarity
