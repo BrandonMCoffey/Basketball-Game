e6603981070bd88bea31d8d707cb9364e5e15c51
BrandonMCoffey <bmc939@gmail.com>
2025-12-08 23:43:25 -0600

    hehe silly

--- Assets/BasketballTricks/Scripts/PlayerManager.cs
+++ Assets/BasketballTricks/Scripts/PlayerManager.cs
@@ -329,13 +329,10 @@ public class PlayerManager : MonoBehaviour
             else
             {
                 // TODO: Highlight cards
-                //Debug.Log($"For {action.Name}: {sequenceCost} + {player.CardData.GetAction(actionIndex).Effects.Cost} + {adjustCost} out of {_maxEnergy}");
+                Debug.Log($"For {action.Name}: {sequenceCost} + {player.CardData.GetAction(actionIndex).Effects.Cost} + {adjustCost} out of {_maxEnergy}");
                 bool locked = _maxEnergy < sequenceCost + player.CardData.GetAction(actionIndex).Effects.Cost + adjustCost;
+                cards[i - TimelineActions.Count].RefreshVisuals(adjustCost, adjustHype);
                 cards[i - TimelineActions.Count].SetLocked(locked);
-                if (!locked)
-                {
-                    cards[i - TimelineActions.Count].RefreshVisuals(adjustCost, adjustHype);
-                }
             }
         }
         for (; j < _actionVisualPreviews.Count; j++)
