a893348506132ff8c82257148471fc667f0dd21a
Sai Mangipudi <thegeekskm101@gmail.com>
2025-12-08 22:58:50 -0600

    why hath god forsaken me

--- Assets/BasketballTricks/Scripts/UI/HypeScoreDisplay.cs
+++ Assets/BasketballTricks/Scripts/UI/HypeScoreDisplay.cs
@@ -43,13 +43,15 @@ public class HypeScoreDisplay : MonoBehaviour
             _hypeText.transform.localScale = Vector3.one;
             _hypeText.transform.localRotation = Quaternion.identity;
 
-            _rectTransform.DOAnchorPosY(20, 0.1f).SetEase(Ease.OutQuad);
-            _hypeText.transform.DORotate(Vector3.one * Random.Range(-15f, 15f), 0.1f).SetEase(Ease.OutQuad);
+            _rectTransform.DOAnchorPosY(5, 0.1f).SetEase(Ease.OutQuad);
+            _rectTransform.DOScale(Vector3.one * 1.2f, 0.1f).SetEase(Ease.OutQuad);
+            _hypeText.transform.DORotate(Vector3.one * Random.Range(-5f, 5f), 0.1f).SetEase(Ease.OutQuad);
             _hypeText.transform.DOScale(Vector3.one * 1.2f, 0.1f).SetEase(Ease.OutQuad).OnComplete(() =>
             {
-                _rectTransform.DOAnchorPosY(_currentAnchorPosY, 0.2f).SetEase(Ease.OutBack).SetDelay(0.15f);
-                _hypeText.transform.DOScale(Vector3.one, 0.2f).SetEase(Ease.OutBack).SetDelay(0.15f);
-                _hypeText.transform.DORotate(Vector3.zero, 0.2f).SetEase(Ease.OutBack).SetDelay(0.15f);
+                _rectTransform.DOAnchorPosY(_currentAnchorPosY, 0.2f).SetEase(Ease.OutBack);
+                _rectTransform.DOScale(Vector3.one, 0.2f).SetEase(Ease.OutBack);
+                _hypeText.transform.DOScale(Vector3.one, 0.2f).SetEase(Ease.OutBack);
+                _hypeText.transform.DORotate(Vector3.zero, 0.2f).SetEase(Ease.OutBack);
             });
         }
     }
