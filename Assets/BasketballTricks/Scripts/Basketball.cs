5d8e63c1d67e8f28af25b9d6ebc6aad98299811b
Coffey <bmc180001@utdallas.edu>
2025-12-11 11:55:17 -0600

    Bounce

--- Assets/BasketballTricks/Scripts/Basketball.cs
+++ Assets/BasketballTricks/Scripts/Basketball.cs
@@ -1,5 +1,31 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Basketball : MonoBehaviour
 {
+    [SerializeField] private float _yThreshold = 0.1f;
+    [SerializeField] private UnityEvent _eventOnBounce = new UnityEvent();
+
+    private bool _grounded;
+
+    private void Start()
+    {
+        _grounded = transform.position.y <= _yThreshold;
+    }
+
+    public void Update()
+    {
+        bool grounded = transform.position.y <= _yThreshold;
+        if (grounded && !_grounded)
+        {
+            OnBounce();
+        }
+        _grounded = grounded;
+    }
+
+    private void OnBounce()
+    {
+        //SFXManager.PlaySfx(SFXEventEnum.BasketballBounce);
+        _eventOnBounce?.Invoke();
+    }
 }
