12fd52e64bc8032f59d3b37cd289a50ec0817ba5
Coffey <bmc180001@utdallas.edu>
2025-12-11 17:57:15 -0600

    Project Cleanup

--- Assets/BalatroGame/Scripts/Data/Bosses/Boss_DoubleScoreRequirement.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BE_DoubleScore", menuName = "Balatro/Boss Effects/Double Score Requirement")]
-public class DoubleScoreRequirementEffect : BossEffect
-{
-    // No logic needed
-}
--- Assets/BalatroGame/Scripts/Data/OpponentNameList.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "Opponent Name List", menuName = "Balatro/Opponent Name List")]
-public class OpponentNameList : ScriptableObject
-{
-    public List<string> Names;
-}
